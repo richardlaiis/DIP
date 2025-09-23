092ec35ff497c17adabdb2cfce6c29278dde8fbc
richardlaiis <richardlaiis@proton.me>
2025-09-23 22:57:44 +0800

    initial commit

---
+++ HW1/scale.py
@@ -0,0 +1,17 @@
+import cv2 as cv
+import numpy as np
+try:
+    scale = float(input())
+except:
+    print('Invalid Input: Number is expected')
+filename = 'middle_finger.jpg'
+
+img = cv.imread(filename)
+height, width = img.shape[0], img.shape[1]
+
+height = int(scale * height)
+width = int(scale * width)
+
+result = cv.resize(img, (width, height), interpolation = cv.INTER_CUBIC)
+# or INTER_LINEAR
+cv.imwrite(f'{scale}_{filename}', result)
